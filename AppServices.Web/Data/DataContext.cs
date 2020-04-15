f14ed2163f92fdb488c114ad1ae26c592a25df2e
ANDRES FELIPE PANIAGUA LEMA <andrespaniagua250958@correo.itm.edu.co>
2020-04-15 17:05:14 -0500

    Define folders

---
+++ AppServices.Web/Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using AppServices.Web.Data.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AppServices.Web.Data
+{
+    public class DataContext : IdentityDbContext<UserEntity>
+    {
+        public DataContext(DbContextOptions<DataContext> options)
+            : base(options)
+        {
+
+        }
+    }
+}
---
+++ AppServices.Web/Data/Entities/UserEntity.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace AppServices.Web.Data.Entities
+{
+    public class UserEntity : IdentityUser
+    {
+        [Display(Name = "Document")]
+        [MaxLength(20, ErrorMessage = "The {0} field can not have more than {1} characters.")]
+        [Required(ErrorMessage = "The field {0} is mandatory.")]
+        public string Document { get; set; }
+
+        [Display(Name = "First Name")]
+        [MaxLength(50, ErrorMessage = "The {0} field can not have more than {1} characters.")]
+        [Required(ErrorMessage = "The field {0} is mandatory.")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
+        [MaxLength(50, ErrorMessage = "The {0} field can not have more than {1} characters.")]
+        [Required(ErrorMessage = "The field {0} is mandatory.")]
+        public string LastName { get; set; }
+
+        //TypeUser
+
+        [MaxLength(100, ErrorMessage = "The {0} field can not have more than {1} characters.")]
+        public string Address { get; set; }
+
+        public string FullName => $"{FirstName} {LastName}";
+
+        public string FullNameWithDocument => $"{FirstName} {LastName} - {Document}";
+    }
+}
