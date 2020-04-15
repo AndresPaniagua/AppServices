5befb5cd6e22e0c6935f2d030a33d650c12781f8
Andres <Other@LAPTOP-OAPHO21I>
2020-04-15 18:12:01 -0500

    ServiceEntity and ServiceTypeEntity

---
+++ AppServices.Web/Data/Entities/ServiceEntity.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppServices.Web.Data.Entities
+{
+    public class ServiceEntity
+    {
+        public int Id { get; set; }
+
+        [MaxLength(30, ErrorMessage = "The {0} field can not have more than {1} characters.")]
+        public string ServicesName { get; set; }
+
+        [MaxLength(11, ErrorMessage = "The {0} field can not have more than {1} characters.")]
+        public string Phone { get; set; }
+
+        [DataType(DataType.DateTime)]
+        [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd hh:mm}", ApplyFormatInEditMode = false)]
+        public DateTime StartDate { get; set; }
+
+        [DataType(DataType.DateTime)]
+        [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd hh:mm}", ApplyFormatInEditMode = false)]
+        public DateTime StartDateLocal => StartDate.ToLocalTime();
+
+        [DataType(DataType.DateTime)]
+        [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd hh:mm}", ApplyFormatInEditMode = false)]
+        public DateTime FinishDate { get; set; }
+
+        [DataType(DataType.DateTime)]
+        [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd hh:mm}", ApplyFormatInEditMode = false)]
+        public DateTime FinishDateLocal => FinishDate.ToLocalTime();
+
+        [MaxLength(500, ErrorMessage = "The {0} field can not have more than {1} characters.")]
+        public string Description { get; set; }
+
+        public decimal Price { get; set; }
+
+        public ServiceTypeEntity ServiceType { get; set; }
+
+
+    }
+}
---
+++ AppServices.Web/Data/Entities/ServiceTypeEntity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppServices.Web.Data.Entities
+{
+    public class ServiceTypeEntity
+    {
+        public int Id { get; set; }
+
+        [MaxLength(30, ErrorMessage = "The {0} field can not have more than {1} characters.")]
+        public string Name { get; set; }
+
+        public ICollection<ServiceEntity> Services { get; set; }
+
+    }
+}
